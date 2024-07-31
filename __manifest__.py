06a9dfd4a666865845b8a7b1246a0fb3687670c8
bipin1050 <bipin.khanal1050@gmail.com>
2024-07-31 13:59:36 +0545

    feat: hello world template

--- __manifest__.py
+++ __manifest__.py
@@ -18,11 +18,12 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'website'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'views/snippets/s_search_bar.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
